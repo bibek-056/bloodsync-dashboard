f166142d218351cc815499373c0f9dbb59067f83
bibek-056 <adh.bibek056@gmail.com>
2023-10-13 12:07:17 +0545

    merge: merging two branches

--- src/pages/donor.tsx
+++ src/pages/donor.tsx
@@ -10,20 +10,19 @@ import TableRow from '@mui/material/TableRow';
 import Loading from '../components/Loading';
 import DeleteIcon from '@mui/icons-material/Delete';
 import BorderColorIcon from '@mui/icons-material/BorderColor';
-import {
-  useDeleteRequestMutation,
-  useReadRequestQuery,
-} from '../api/apiHandler';
 
 import {
-  Button,
+  useReadRequestQuery,
+  useDeleteRequestMutation,
+} from '../api/apiHandler';
+import {
   Dialog,
   DialogActions,
   DialogContent,
   DialogContentText,
   DialogTitle,
 } from '@mui/material';
-import { IoMdAddCircleOutline } from 'react-icons/io';
+import { Button } from 'antd';
 
 interface Column {
   id:
@@ -209,105 +208,90 @@ function Donor() {
   return isLoading ? (
     <Loading />
   ) : (
-    <>
-      <div className="flex w-[100%] justify-between items-center">
-        <input
-          placeholder="Search Here"
-          className="w-2/5 h-12 p-4 rounded border"
-        ></input>
-        {/* <BigButton/> */}
-        <button
-          className="flex items-center justify-center gap-2 border w-64 h-12 rounded p-4 bg-green-500 text-white font-medium m-5"
-          // onClick={handleOpenForm}
-        >
-          <IoMdAddCircleOutline className="text-lg" /> Add New Donor
-        </button>
-      </div>
-      <Paper sx={{ width: 'auto', backgroundColor: '#F1F5F9' }}>
-        <TableContainer
-          style={{
-            backgroundColor: '#F1F5F9',
-          }}
-        >
-          <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-              <TableRow>
-                {columns.map((column, index) => (
-                  <TableCell
-                    key={index}
-                    align={column.align}
-                    style={{
-                      // minWidth: column.minWidth,
-                      backgroundColor: '#F1F5F9',
-                    }}
+    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer
+        style={{
+          backgroundColor: '#F1F5F9',
+        }}
+      >
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{
+                    minWidth: column.minWidth,
+                    backgroundColor: '#F1F5F9',
+                  }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row, index: number) => {
+                return (
+                  <TableRow
+                    hover
+                    // tabIndex={-1}
+                    key={row.index}
+                    className={index % 2 === 0 ? 'bg-white' : 'bg-slate-100'}
                   >
-                    {column.label}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows && rows
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row: any, index: number) => {
-                  return (
-                    <TableRow
-                      hover
-                      // tabIndex={-1}
-                      key={row.index}
-                      className={index % 2 === 0 ? 'bg-white' : 'bg-slate-100'}
-                    >
-                      {columns.map((column, index) => {
-                        const value = row[column.id];
-                        return (
-                          <TableCell key={index} align={column.align}>
-                            {value}
-                          </TableCell>
-                        );
-                      })}
-                    </TableRow>
-                  );
-                })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          className="bg-slate-100"
-          rowsPerPageOptions={[10, 25, 100]}
-          component="div"
-          count={rows?.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-        <Dialog
-          open={openDrawer}
-          onClose={handleCloseDrawer}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">Delete Record</DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              Are you sure you want to delete this record?
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleCloseDrawer}>Cancel</Button>
-            <Button
-              onClick={() => {
-                handleCloseDrawer(); // Close the dialog
-                // Handle delete here
-              }}
-              autoFocus
-            >
-              Delete
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </Paper>
-    </>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        className="bg-slate-100"
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+      <Dialog
+        open={openDrawer}
+        onClose={handleCloseDrawer}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">Delete Record</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to delete this record?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDrawer}>Cancel</Button>
+          <Button
+            onClick={() => {
+              handleCloseDrawer(); // Close the dialog
+              // Handle delete here
+            }}
+            autoFocus
+          >
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Paper>
   );
 }
 
