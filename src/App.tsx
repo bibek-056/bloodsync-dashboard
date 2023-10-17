21015dbfa93e6665e082c5cc11bde3d7074ae251
Samiksha <swabhimankhadka2001@gmail.com>
2023-10-17 11:28:44 +0545

    fix(route): route for admin page

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import EditDonor from './components/Forms/EditDonor';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import HospitalProfile from './pages/HospitalProfile';
+import Admin from './pages/Admin';
 
 const App = () => (
   <Router>
@@ -24,6 +25,7 @@ const App = () => (
         <Route path="/create-donor" element={<CreateDonor />} />
         <Route path="/donor/edit/:donorId" element={<EditDonor />} />
         <Route path="/hospitalProfile/:hospitalId" element={<EditDonor />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </Navbar>
     <ToastContainer />
